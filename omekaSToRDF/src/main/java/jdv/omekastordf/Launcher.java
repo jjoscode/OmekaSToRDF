856cb72e0d1d1febfa2fa6338bfafd1607e7ad41
jjoscode <jojodv02@gmail.com>
2025-11-04 16:10:26 +0100

    Delete omekaSToRDF directory

--- omekaSToRDF/src/main/java/jdv/omekastordf/Launcher.java
+++
@@ -1,9 +0,0 @@
-package jdv.omekastordf;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(OmekaToRDFApplication.class, args);
-    }
-}
